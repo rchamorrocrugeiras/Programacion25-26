cbc3d4e538b0b75c29e2347a72ecc67dba319817
René <rchamorrocrugeiras@danielcastelao.org>
2025-10-09 14:07:45 +0200

    Boletin1 ejercicio4

---
+++ Boletin1/Ejercicio4.py
@@ -0,0 +1,17 @@
+# 4. Avalia as seguintes expresións:
+
+# a) True and True == False
+# b) not False == True
+# c) (True and True) or False == True
+# d) (False or False) and False != True
+# e) (not(True and False)) == False
+# f) “12” + “12” == “24”
+# g) “34” + “43” == “3443”
+
+print("a)", True and True == False) # False
+print("b)", not False == True) # True
+print("c)", (True and True) or False == True) # True
+print("d)", (False or False) and False != True) # False
+print("e)", (not(True and False)) == False) # False
+print("f)", "12" + "12" == "24") # False
+print("g)", "34" + "43" == "3443") # True
