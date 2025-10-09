2e276393c12bd9a54a83ff53141a1f0f44c3e622
René <rchamorrocrugeiras@danielcastelao.org>
2025-10-09 13:24:43 +0200

    Boletin1 ejercicio2

---
+++ Boletin1/Ejercicio2.py
@@ -0,0 +1,16 @@
+# 2. Cales dos seguintes nomes de variables non son válidos?
+# a) Salto- mortal , salto_mortal, salto + mortal, 2salto, “salto”
+# b) cantidade total, cant_total, cant5, cantidadeTotal
+
+Salto- mortal == 10
+salto_mortal == 20
+salto + mortal == 30
+2salto == 40
+"salto" = 50
+# Variable válida: salto_mortal
+
+cantidade total == 100
+cant_total == 200
+cant5 == 300
+cantidadeTotal == 400
+# Variables válidas: cant_total, cant5, cantidadeTotal
