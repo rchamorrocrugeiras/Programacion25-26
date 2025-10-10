1b514ce0fabca4f57b46b98368f7abe537fbaa57
René <rchamorrocrugeiras@danielcastelao.org>
2025-10-10 09:20:33 +0200

    Boletin2 Ejercicio3

---
+++ Boletin2/Ejercicio3.py
@@ -0,0 +1,8 @@
+# Programa que cambia euros a dólares
+
+euros = float(input("Introduce a cantidade en euros: "))
+cambio = 1.16
+
+dolares = euros * cambio
+
+print("O equivalente en dólares é:", dolares)
