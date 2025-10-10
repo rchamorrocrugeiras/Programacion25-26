65ee9bffbda69cf35367bcc7f13c20590c42a821
René <rchamorrocrugeiras@danielcastelao.org>
2025-10-10 09:29:52 +0200

    Boletin2 Ejercicio5

---
+++ Boletin2/Ejercicio5.py
@@ -0,0 +1,7 @@
+# Programa que converte millas mariñas a metros
+
+millas = float(input("Introduce a distancia en millas mariñas: "))
+
+metros = millas * 1852
+
+print(millas, "millas mariñas equivalen a", metros, "metros")
