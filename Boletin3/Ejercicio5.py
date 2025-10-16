a2a3fdecf44c7d43fd9ed05f828f0b19f6415897
René <rchamorrocrugeiras@danielcastelao.org>
2025-10-16 13:21:51 +0200

    Boletin3 Ejercicio5

---
+++ Boletin3/Ejercicio5.py
@@ -0,0 +1,12 @@
+# Programa que dados 3 números distintos indique cal é o maior
+
+num1 = 5
+num2 = 8
+num3 = 3
+
+if num1 > num2 and num1 >num3:
+    print ("O maior número é:", num1)
+if num2 > num1 and num2 >num3:
+    print ("O maior número é:", num2)
+if num3 > num1 and num3 > num2:
+    print ("O maior número é:", num3)
