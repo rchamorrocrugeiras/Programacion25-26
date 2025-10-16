df1864f187b5003af29412ef68a30a0532876228
René <rchamorrocrugeiras@danielcastelao.org>
2025-10-16 13:02:55 +0200

    Boletin3 Ejercicio3

---
+++ Boletin3/Ejercicio3.py
@@ -0,0 +1,12 @@
+# Programa que mostre por consola o tipo de número que é
+
+num = float(input("Introduce un número:"))
+
+if num > 0:
+    print ("+")
+
+if num < 0:
+    print ("-")
+
+if num == 0:
+    print ("0")
