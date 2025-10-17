1020009e412ee299e4dcc1c61ff396d69e73e7a9
René <rchamorrocrugeiras@danielcastelao.org>
2025-10-17 08:48:50 +0200

    Boletin4 Ejercicio3

---
+++ Boletin4/Ejercicio3.py
@@ -0,0 +1,7 @@
+# Programa que utilizando o operador terniario calcula o valor absoluto dun número
+
+num = float(input("Introduce un número: "))
+
+valor_absoluto = num if num >= 0 else -num
+
+print ("O valor absoluto é:", valor_absoluto)
