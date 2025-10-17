2df2babab52034938e19636509d9b5fa6f7889f3
René <rchamorrocrugeiras@danielcastelao.org>
2025-10-17 09:34:06 +0200

    Ahorcado fixed

--- Ejercicios de clase/Ahorcado.py
+++ Ejercicios de clase/Ahorcado.py
@@ -5,6 +5,52 @@ palabras = ["python", "ahorcado", "programacion", "juego", "computadora", "tecla
 palabra = random.choice(palabras)
 letras_adivinadas = []
 intentos = 7
+fallos = 0
+
+def debuxa_boneco(fallos):
+    if fallos == 1:
+        for i in range(6):
+            print("\n")
+        print("        -------")
+    elif fallos == 2:
+        print("\n")
+        for i in range(1, 6):
+            print("           |")
+        print("        -------")
+    elif fallos == 3:
+        print("           _________")
+        for i in range(1, 6):
+            print("           |")
+        print("        -------")
+    elif fallos == 4:
+        print("           _________")
+        print("           |       |")
+        for i in range(2, 6):
+            print("           |")
+        print("        -------")
+    elif fallos == 5:
+        print("           _________")
+        print("           |       |")
+        print("           |       O")
+        for i in range(3, 6):
+            print("           |")
+        print("        -------")
+    elif fallos == 6:
+        print("           _________")
+        print("           |       |")
+        print("           |       O")
+        print("           |      /|\\")
+        print("           |      ")
+        print("           |      ")
+        print("        -------")
+    elif fallos == 7:
+        print("           _________")
+        print("           |       |")
+        print("           |       O")
+        print("           |      /|\\")
+        print("           |      / \\")
+        print("           |      ")
+        print("        -------")
 
 print("Benvido ao xogo do Ahorcado!")
 print("Adiviña a palabra secreta.")
@@ -17,7 +63,7 @@ while intentos > 0:
         else:
             palabra_mostrada += "_"
 
-    print("Palabra:", palabra_mostrada)
+    print("\nPalabra:", palabra_mostrada)
     print("Intentos restantes:", intentos)
 
     if "_" not in palabra_mostrada:
@@ -41,6 +87,8 @@ while intentos > 0:
     else:
         print("A letra non está na palabra.")
         intentos -= 1
+        fallos += 1
+        debuxa_boneco(fallos)
 
 if intentos == 0:
     print("Perdeches! A palabra era:", palabra)
