3fcaa2884be8eaf50044eb16d1704a96c3688acb
René <rchamorrocrugeiras@danielcastelao.org>
2025-10-17 09:12:36 +0200

    Boletin5 Ejercicio2

---
+++ Boletin5/Ejercicio2.py
@@ -0,0 +1,7 @@
+# Programa que pasa de grados Fahrenheit a Celsius
+
+fahrenheit = float(input("Introduce os grados Fahrenheit:"))
+
+celsius = (5/9) * (fahrenheit - 32)
+
+print ("A temperatura en Celsius é:", celsius, "ºC")
