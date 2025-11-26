32c7ef33ec012fc2e1a92ee84a832650f6ade9f4
René <rchamorrocrugeiras@danielcastelao.org>
2025-11-26 13:29:09 +0100

    Boletin6 Ejercicio8

---
+++ Boletin6/Ejercicio8.py
@@ -0,0 +1,9 @@
+# Programa que almacene en una lista unos precios y muestre por pantalla el menor y el mayor de los precios
+
+precios = [50, 75, 46, 22, 80, 65, 8]
+
+precio_menor = min(precios)
+precio_mayor = max(precios)
+
+print(f"El menor precio es {precio_menor}")
+print(f"El mayor precio es {precio_mayor}")
