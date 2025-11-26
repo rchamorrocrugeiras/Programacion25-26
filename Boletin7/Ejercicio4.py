664d9ca0505fc665602e1c29a28113eac38c25b9
René <rchamorrocrugeiras@danielcastelao.org>
2025-11-26 14:06:03 +0100

    Boletin7 Ejercicio4

---
+++ Boletin7/Ejercicio4.py
@@ -0,0 +1,7 @@
+# Programa que elimina los espacios del texto "Guido Van Rossum creou Python"
+
+texto = "Guido Van Rossum creou Python"
+
+texto_sin_espacios = texto.replace(" ", "")
+
+print(texto_sin_espacios)
