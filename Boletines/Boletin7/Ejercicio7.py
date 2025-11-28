7218136b12193ff0d5883af9cc723a274008c5fb
René <rchamorrocrugeiras@danielcastelao.org>
2025-11-28 09:07:21 +0100

    Boletin7 Ejercicio7

---
+++ Boletines/Boletin7/Ejercicio7.py
@@ -0,0 +1,9 @@
+# Programa que transforma la cadena de texto "Pythoneros" a mayúsculas, guarda el valor en una variable
+# y posteriormente la convierte nuevamente a minúsculas
+
+texto = "Pythoneros"
+texto_mayúsculas = texto.upper()
+texto_minúsculas = texto_mayúsculas.lower()
+
+print(f"En mayúsculas: {texto_mayúsculas}")
+print(f"En minúsculas: {texto_minúsculas}")
