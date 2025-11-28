0d442480a1b3f1646e4db606d8943c0fb88cad40
René <rchamorrocrugeiras@danielcastelao.org>
2025-11-28 09:02:27 +0100

    Boletin7 Ejercicio6

---
+++ Boletines/Boletin7/Ejercicio6.py
@@ -0,0 +1,12 @@
+# Programa que divida una cadena de texto en dos partes, para luego concatenarlas y mostrarlas de nuevo
+
+texto = " www. phytonparatodos. com"
+
+parte1 = "www. phyton"
+parte2 = "paratodos. com"
+
+texto_entero = parte1 + parte2
+
+print("Parte 1:", parte1)
+print("Parte 2:", parte2)
+print("Texto final:", texto_entero)
