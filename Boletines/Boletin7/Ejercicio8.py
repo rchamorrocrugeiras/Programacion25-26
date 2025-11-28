80d77e3562bd78dee8748c33fdc24bc1d94ac53e
René <rchamorrocrugeiras@danielcastelao.org>
2025-11-28 09:10:48 +0100

    Boletin7 Ejercicio8

---
+++ Boletines/Boletin7/Ejercicio8.py
@@ -0,0 +1,9 @@
+# Programa que compare si el String "Python" es igual al String "JavaScript"
+
+texto1 = "Python"
+texto2 = "JavaSccript"
+
+if texto1 == texto2:
+    print("Son iguales")
+else:
+    print("Son diferentes")
