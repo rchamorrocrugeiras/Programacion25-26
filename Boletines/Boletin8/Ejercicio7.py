1645eb4ecb9f5106c1b8f33afd5abe9f2a125bed
René <rchamorrocrugeiras@danielcastelao.org>
2025-12-04 13:22:18 +0100

    Boletin8 Ejercicio7

--- Boletines/Boletin8/Ejercicio7.py
+++ Boletines/Boletin8/Ejercicio7.py
@@ -2,3 +2,32 @@
 # Devuelva tres listas, una con los menores, otra con los mayores y otra con los iguales a k.
 # Devuelva una lista con aquellos que son múltiplos de k.
 
+def clasificar_por_k(lista, k):
+    menores = []
+    iguales = []
+    mayores = []
+    multiplos = []
+
+    for num in lista:
+        if num < k:
+            menores.append(num)
+        elif num > k:
+            mayores.append(num)
+        else:
+            iguales.append(num)
+
+        if k != 0 and num % k == 0:
+            multiplos.append(num)
+
+    return menores, iguales, mayores, multiplos
+
+
+lista_numeros = [3, 7, 10, 2, 15, 9, 5, 20]
+k = 5
+
+menores, iguales, mayores, multiplos = clasificar_por_k(lista_numeros, k)
+
+print("Menores que k:", menores)
+print("Iguales a k:", iguales)
+print("Mayores que k:", mayores)
+print("Múltiplos de k:", multiplos)
