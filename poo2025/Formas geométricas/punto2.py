c10cac3f75abb84ed5163719d4bb3b4f8f2cbf8c
René <rchamorrocrugeiras@danielcastelao.org>
2025-12-04 13:13:16 +0100

    Formas geométricas punto2

---
+++ poo2025/Formas geométricas/punto2.py
@@ -0,0 +1,41 @@
+class Punto2:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def setX(self, x):
+        if type(x) == int or type(x) == float:
+            if x >= 0:
+                self.__x = x
+            else:
+                self.__x = 0
+        else:
+            self.__x = 0
+
+    def setY(self, y):
+        if type(y) == int or type(y) == float:
+            if y >= 0:
+                self.__y = y
+            else:
+                self.__y = 0
+        else:
+            self.__y = 0
+
+    def getX(self):
+        return self.__x
+
+    def getY(self):
+        return self.__y
+
+    def toString(self):
+        cadeaPunto = "As coordenadas do punto son: \n\t x = " + str(self.__x) + " \n\t y = " + str(self.__y)
+        return cadeaPunto
+
+    def __str__(self):
+        return self.toString()
+
+    def __eq__(self, other):
+        return self.__x == other.x and self.__y == other.y
+
+    x=property(getX, setX)
+    y=property(getY, setY)
