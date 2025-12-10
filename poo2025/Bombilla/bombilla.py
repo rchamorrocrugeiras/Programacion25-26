a3965f934b316bace812fe12e7785d5c063390ec
René <rchamorrocrugeiras@danielcastelao.org>
2025-12-10 13:14:13 +0100

    fix Ejercicio poo bombilla

--- poo2025/Bombilla/bombilla.py
+++ poo2025/Bombilla/bombilla.py
@@ -1,16 +1,14 @@
 class Bombilla():
-    def __init__(self, ):
+    def __init__(self):
         self.__encendida = False
 
 
 # Métodos
     def enciende(self):
         self.__encendida = True
-        print("La bombilla está encendida.")
 
     def apaga(self):
         self.__encendida = False
-        print("La bombilla está apagada.")
 
     def estado(self):
         return "Encendida" if self.__encendida else "Apagada"
