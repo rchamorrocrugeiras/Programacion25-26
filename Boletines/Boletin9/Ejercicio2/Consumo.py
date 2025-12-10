f76fd2ce8b1f044b921a64c77645fd64058cf6fb
René <rchamorrocrugeiras@danielcastelao.org>
2025-12-10 13:15:08 +0100

    Creación Boletin9 Ejercicio2

---
+++ Boletines/Boletin9/Ejercicio2/Consumo.py
@@ -0,0 +1,8 @@
+class consumo():
+    def __init__(self, km, litros, vMed, pGas):
+        self.__km = km
+        self.__litros = litros
+        self.__vMed = vMed
+        self.__pGas = pGas
+    
+    
