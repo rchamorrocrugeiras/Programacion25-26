46e4ba2281a3576e8eabb67e9f840d5bb139227c
René <rchamorrocrugeiras@danielcastelao.org>
2025-12-10 13:33:54 +0100

    fix Ejercicio poo bombilla pruebas

--- poo2025/Bombilla/bombilla.py
+++ poo2025/Bombilla/bombilla.py
@@ -13,17 +13,22 @@ class Bombilla():
     def estado(self):
         return self.__encendida
 
+    def conmuta(self):
+        self.__encendida = not self.__encendida
+
     def __str__(self):
-        return "Encendida" if self.__encendida else "Apagada"
+        return "La bombilla está encendida" if self.__encendida else "La bombilla está apagada"
 
 
 # Pruebas
 b1 = Bombilla()
+b2 = Bombilla()
 
-print("Estado inicial:", b1.__str__())
-
+print(b1)
+print(b2)
 b1.enciende()
-print("Estado actual:", b1.__str__())
-
-b1.apaga()
-print("Estado actual:", b1.__str__())
+print(b1)
+print(b2)
+b1.conmuta()
+print(b1)
+print(b2)
