04087b05343cfac50db6034803f56afe2b91112c
ReneChamorro <rchamorrocrugeiras@danielcastelao.org>
2026-03-26 13:05:32 +0100

    Comienzos con Java

---
+++ Java/ejemplosConversion.java
@@ -0,0 +1,16 @@
+public class ConversionTipos {
+
+    public static void main (String [] args){
+        int numero = 511;
+        byte numPequeno = (byte) numero;
+
+        System.out.println (numPequeno);
+
+
+    }
+
+
+
+
+
+}
