cda7e7e3f2eb0bc2a4919cbbb73bb9142d3eee75
ReneChamorro <rchamorrocrugeiras@danielcastelao.org>
2026-03-26 13:35:01 +0100

    Pruebas con java

---
+++ Java/ConversionTipos.java
@@ -0,0 +1,12 @@
+public class ConversionTipos {
+    public static void main (String[] args){
+        int numero = 257;
+        byte numPequeno = (byte) numero;
+        System.out.println(numPequeno);
+
+        int n1 = 55555;
+        long n2 = 66666;
+        long n3 = n1 * n2;
+        System.out.println(n3);
+    }
+}
