01634914e2a94828abc2cac8af4c51aaf6f6142d
ReneChamorro <rchamorrocrugeiras@danielcastelao.org>
2026-04-10 11:44:46 +0200

    retocando clases java

--- Java/MembroComunidadeEscolar.java
+++ Java/MembroComunidadeEscolar.java
@@ -7,4 +7,20 @@ public class MembroComunidadeEscolar extends Persoa{
         this.codigoCentro = codCentro;
         this.nomeCentro = nomCentro;
     }
+
+    public String getCodigoCentro() {
+        return this.codigoCentro;
+    }
+
+    public void setCodigoCentro(int novoCodCentro) {
+        if (novoCodCentro > 0) codigoCentro = novoCodCentro;
+        else codigoCentro = 0;
+    }
+
+    public String getNomeCentro() {
+        return nomeCentro;
+    }
+
+    public void setNomeCentro(String nomeCentro) {
+        this.nomeCentro = nomeCentro;
 }
