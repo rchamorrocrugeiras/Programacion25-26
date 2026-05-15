6a58e068884ef349f3ce4d0c49c7ebadf913746e
ReneChamorro <rchamorrocrugeiras@danielcastelao.org>
2026-05-15 10:35:56 +0200

    Boletin13 Ejercicio2

---
+++ Java/Boletines/Ejercicio2.java
@@ -0,0 +1,23 @@
+package Boletines;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            numeros.add(random.nextInt(10) + 1);
+        }
+
+        System.out.println("Antes de eliminar:");
+        System.out.println(numeros);
+
+        numeros.removeIf(n -> n == 5 || n == 7);
+
+        System.out.println("\nDespois de eliminar:");
+        System.out.println(numeros);
+    }
+}
