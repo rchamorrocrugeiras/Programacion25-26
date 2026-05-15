a1800558365beed775255831fbf00ff08cd18716
ReneChamorro <rchamorrocrugeiras@danielcastelao.org>
2026-05-15 10:58:21 +0200

    Arreglos en Boletin13 Ejercicio2

--- Java/Boletines/Ejercicio2.java
+++ Java/Boletines/Ejercicio2.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Ejercicio2 {
-    public static void main(String[] args) {
-        ArrayList<Integer> numeros = new ArrayList<>();
+    public static void main(String[] args){
+        ArrayList<Integer> lista = new ArrayList<>();
         Random random = new Random();
 
-        for (int i = 0; i < 100; i++) {
-            numeros.add(random.nextInt(10) + 1);
+        for (int i = 0; i < 100; i++){
+            lista.add(random.nextInt(10) + 1);
         }
 
         System.out.println("Antes de eliminar:");
-        System.out.println(numeros);
+        System.out.println(lista);
 
-        numeros.removeIf(n -> n == 5 || n == 7);
+        lista.removeIf(n -> n == 5 || n == 7);
 
         System.out.println("\nDespois de eliminar:");
-        System.out.println(numeros);
+        System.out.println(lista);
     }
 }
